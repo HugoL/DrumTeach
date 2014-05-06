795832cdc53c40256f71b6edaf54a19e62e95646
Hugo Langa <hugoepila@gmail.com>
2014-05-07 01:21:26 +0200

    Algunos cambios en la apariencia

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -7,14 +7,14 @@ $this->pageTitle=Yii::app()->name;
 <?php $this->beginWidget('bootstrap.widgets.TbHeroUnit', array(
     'heading'=>CHtml::encode(Yii::app()->name),
 )); ?>
- 
+	<div class="clearfix">&nbsp;</div>
     <p>Aplicación web que te permite estar al corriente de tus progresos en tus clases de batería.</p>
 
     <p><strong>Totalmente gratuita.</strong> Solo tienes que registrarte y ya la puedes disfrutar.</p>
     
     <div class="clearfix">&nbsp;</div>
     <center><p><?php $this->widget('bootstrap.widgets.TbButton', array(
-        'type'=>'inverse',
+        'type'=>'warning',       
         'size'=>'large',
         'label'=>'¡Regístrate gratis!',
     )); ?></p></center>
--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -7,7 +7,7 @@ $this->breadcrumbs=array(
 );
 ?>
 <h1>Sobre DrumTeach</h1>
-
+<div class="texto">
 <p>DrumTeach es un proyecto personal que me permite estar al corriente de mis progresos con mis clases de batería.</p>
 
 <p>El único propósito por el que desarrollo esta aplicación es para poder tener la información que necesito, relacionada con mis clases de batería, en cualquier momento y en cualquier lugar, gracias a la presencia de los smartphones, que hoy en día te permiten conectarte a la red desde casi cualquier sitio</p>
@@ -15,5 +15,5 @@ $this->breadcrumbs=array(
 <p>Aunque la aplicación está destinada a mi uso personal, he decido hacerla multiusuario, para que cualquiera la pueda utilizar igual que yo.</p>
 
 <p><strong>Esto es totalmente gratis y totalmente desinteresado.</strong> No pretendo beneficiarme económicamente de esto. No existe modelo de negocio. Si te resulta útil, la usas y ya está ;-)</p>
-
+</div>
 <p></p>
