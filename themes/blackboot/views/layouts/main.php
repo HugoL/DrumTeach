f1fd0768f775fd7ccb5a97952fa5993cf99478ce
Hugo Langa <hugoepila@gmail.com>
2014-05-08 02:38:24 +0200

    Escala 1 por defecto para que se vea mejor en el móvil. Cambiado el texto del index

--- themes/blackboot/views/layouts/main.php
+++ themes/blackboot/views/layouts/main.php
@@ -23,8 +23,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <title><?php echo CHtml::encode($this->pageTitle); ?></title>
-<meta name="language" content="en" />
+<meta name="language" content="es" />
 <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
 <!--[if lt IE 9]>
 <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
